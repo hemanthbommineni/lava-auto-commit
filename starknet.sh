1eb226033134e90a99e1af03268bfac1be529c43
0xfarmed <31967876+hemanthbommineni@users.noreply.github.com>
2024-05-21 15:18:22 -0700

    Update starknet.sh

--- starknet.sh
+++ starknet.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-RPC_URL="https://rpc.starknet.lava.build/lava-referer-8b51600b-b3cc-4893-89ec-5de41f0724db/"
+RPC_URL="https://rpc.starknet.lava.build/lava-referer-4cd09472-7ecc-4ba5-b079-9ce2efeeae8b/"
 
 OUTPUT_DIR="logs"
 
